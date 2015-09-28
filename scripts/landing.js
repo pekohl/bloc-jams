7c4a8860754ef923272e58c5783ac222d434051a
Peter <pekohl@gmail.com>
2015-09-28 15:49:41 -0400

    Assignment 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,20 @@
-var animatePoints = function () {
+var pointsArray = document.getElementsByClassName('point');
 
-        var points = document.getElementsByClassName('point');
-<<<<<<< HEAD
-    
-        for(var i=0; i < points.length; i++) {
-             points[i].style.opacity = 1;
-             points[i].style.transform = "scaleX(1) translateY(0)";
-             points[i].style.msTransform = "scaleX(1) translateY(0)";
-             points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    
-        }
+var revealPoint = function(point) {
+        point.style.opacity=1;
+        point.style.transform="scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
+
+//    for(var i=0; i < pointsArray.length; i++) {
+//        revealPoint(i);            
+//        }
+
 
 //        var revealFirstPoint = function() {
 //         points[0].style.opacity = 1;
@@ -37,9 +42,22 @@ var animatePoints = function () {
 //        revealSecondPoint();
 //        revealThirdPoint();  
 
-};
 
 
-
-
-
+window.onload = function() {
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+// Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    window.addEventListener('scroll', function(event) {
+        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+ 
+ }
