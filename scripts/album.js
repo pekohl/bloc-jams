8bde3ea6cbde6b36ca1c909a868bf1e1dc8d304e
Peter <pekohl@gmail.com>
2015-10-15 16:53:27 -0400

    checkpoint-27-complete

--- scripts/album.js
+++ scripts/album.js
@@ -82,15 +82,69 @@ var setCurrentAlbum = function(album) {
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"><a/>';
-
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"><a/>';
+var currentPlayingSong = null;
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
     
-    songListContainer.addEventListener('mouseover',function(event) {
-        if (event.target.parentElement.className=== 'album-view-song-item') {
-            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    var findParentByClassName = function(element, targetClass) {
+        var currentParent = element.parentElement;
+        while (currentParent.className != targetClass) {
+            currentParent = currentParent.parentElement;
         }
+
+        return currentParent;
+    };
+
+    var getSongItem = function(element){
+        switch (element.className) {
+            case 'album-song-button':
+            case 'ion-play':
+            case 'ion-pause':
+                return findParentByClassName(element, 'song-item-number');
+            case 'album-view-song-item':
+                return element.querySelector('.song-item-number');
+            case 'song-item-title':
+            case 'song-item-duration':
+                return findParentByClassName(element, '.album-view-song-item').querySelector('song-item-number');
+            case 'song-item-number':
+                return element;
+            default:
+                return;
+        }
+    };
+
+    var clickHandler = function(targetElement) {
+        songItem = getSongItem(targetElement);
+
+        if (currentPlayingSong === null){
+            songItem.innerHTML = pauseButtonTemplate;
+            currentPlayingSong = songItem.getAttribute('data-song-number');
+            }
+        };
+
+    songListContainer.addEventListener('mouseover',function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+
+            var songItem = getSongItem(event.target);
+            if (songItem.getAttribute('data-song-number') !== currentPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
+        }
+
+//                } else if (currentPlayingSong === songItem.getAttribute('data-song-number')) {
+//                songItem.innerHTML = playButtonTemplate;
+//                currentPlayingSong = null;
+//        } else if (currentPlayingSong !== songItem.getAttribute('data-song-number')){
+//                var currentPlayingSongElement = document.querySelector('[data-song-number="' + currentPlayingSong + '"]');
+//                currentPlayingSongElement.innerHTML = currentPlayingSongElement.getAttribute('data-song-number');
+//                songItem.innerHTML = pauseButtonTemplate;
+//                currentPlayingSong = songItem.getAttribute('data-song-number');
+//        }
+//
+
     });
     
     var albums = [albumPicasso, albumMarconi, albumBJoel];
@@ -106,11 +160,21 @@ window.onload = function() {
 
     for (i = 0; i < songRows.length; i++) {
         songRows[i].addEventListener('mouseleave', function(event) {
-            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            var songItem = getSongItem(event.target);
+            var songItemNumber = songItem.getAttribute('data-song-number');
+
+            if (songItemNumber !== currentPlayingSong) {
+                songItem.innerHTML = songItemNumber;
+            }
+
         });
+
+        songRows[i].addEventListener('click', function(event) {
+            clickHandler(event.target);
+        });
+
     }
 
 };
 
 
-                
